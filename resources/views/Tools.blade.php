84eeb3fc64b225f0f3bbf4bb3ce506aca5c1f447
Timo1002 <162825846+Timo1002@users.noreply.github.com>
2024-05-24 18:27:49 +0200

    Update Tools.blade.php

--- resources/views/Tools.blade.php
+++ resources/views/Tools.blade.php
@@ -493,7 +493,9 @@
                     while (c.charAt(0)==' ') c = c.substring(1,c.length);
                     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
                 }
-                return null;      
+                return null;
+            }
+      
 </script>
 </body>
 </html>
