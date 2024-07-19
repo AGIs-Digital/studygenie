b9bb5a92cf55df6819444f7c47d047050d9aacaf
hurradieweltgehtunter <hi@florianlenz.com>
2024-07-19 12:12:13 +0200

    add GH action
    update PHP version
    
    use sftp
    
    update again
    
    test
    
    format for v3.1.1
    
    test
    
    test
    
    test
    
    test appleboy upload
    
    test
    
    test
    
    test
    
    extend
    
    .
    
    test again with SamKirlkland FTP upload
    
    .
    
    use node ftp upload
    
    .
    
    add more output
    
    update
    
    fix output
    
    .
    
    .
    
    update
    
    try out ssh commands
    
    try again
    
    test
    
    .
    
    fix htaccess for IONOS hosting
    
    use composer instead of copying vendor files
    
    update node
    
    try again with SamKirkland FTP Upload
    
    .
    
    .
    
    lets use the other ftp tool again
    
    add deleteRemote
    
    set APP_DEBUG
    
    add laravel app key generation
    
    .
    
    deleteRmote by ssh
    
    add composer installation
    
    .
    
    .
    
    remove sql backup
    
    remove .env file also
    
    also copy htaccess

---
+++ ftp-deploy.js
@@ -0,0 +1,62 @@
+const FtpDeploy = require("ftp-deploy");
+const ftpDeploy = new FtpDeploy();
+
+const config = {
+  user: process.env.FTP_USERNAME,
+  password: process.env.FTP_PASSWORD,
+  host: process.env.FTP_SERVER,
+  port: 22, // Standard SFTP Port
+  localRoot: __dirname,
+  remoteRoot: "/",
+  deleteRemote: false,
+  include: [
+    ".env",
+    "app/**",
+    "artisan",
+    "bootstrap/**",
+    "composer.json",
+    "composer.lock",
+    "config/**",
+    "database/**",
+    "lang/**",
+    "public/**",
+    "public/.htaccess",
+    "resources/**",
+    "routes/**",
+    "storage/**",
+  ],
+  exclude: [
+    ".env.example",
+    ".git/**",
+    ".github/**",
+    ".vite.config.js",
+    ".webpack.mix.js",
+    "*.git",
+    "dist/**/*.map",
+    "node_modules/**",
+    "README.md",
+    "tests/**",
+    "vendor/**",
+  ],
+  sftp: true,
+};
+
+// ftpDeploy.on("uploading", function (data) {
+//     console.log(data.totalFilesCount); // total file count being transferred
+//     console.log(data.transferredFileCount); // number of files transferred
+//     console.log(data.filename); // partial path with filename being uploaded
+// });
+
+ftpDeploy.on("uploaded", function (data) {
+    console.log("✅ " + data.filename + " (" + data.transferredFileCount + " / " + data.totalFilesCount + ")");
+});
+
+ftpDeploy.on("upload-error", function (data) {
+    console.log("❌ " . data.err); // data will also include filename, relativePath, and other goodies
+});
+
+ftpDeploy.deploy(config, function(err) {
+  if (err) console.log(err);
+  else console.log("finished");
+});
+
