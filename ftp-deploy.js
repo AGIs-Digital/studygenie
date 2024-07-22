d67a7926e2f7e50bda44f30300c26e3953dce5ed
hurradieweltgehtunter <hi@florianlenz.com>
2024-07-22 10:37:45 +0200

    fix output

--- ftp-deploy.js
+++ ftp-deploy.js
@@ -32,12 +32,11 @@ ftpDeploy.on("uploading", function (data) {
     console.log(data.transferredFileCount); // number of files transferred
     console.log(data.filename); // partial path with filename being uploaded
 });
+
 ftpDeploy.on("uploaded", function (data) {
-    console.log(data); // same data as uploading event
+    console.log("- " + data.filename + " (total files: " + data.totalFilesCount + ")"); // same data as uploading event
 });
-ftpDeploy.on("log", function (data) {
-    console.log(data); // same data as uploading event
-});
+
 ftpDeploy.on("upload-error", function (data) {
     console.log(data.err); // data will also include filename, relativePath, and other goodies
 });
