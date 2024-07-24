cdb375e0e7b74b70a03a7788ef5aac3c34116883
hurradieweltgehtunter <hi@florianlenz.com>
2024-07-24 12:58:12 +0200

    add maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wartungsarbeiten</title>
+    <style>
+        body,
+        html {
+            height: 100%;
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }
+
+        .background {
+            /* Set your background image here */
+            background-image: url('/asset/images/503_bg.jpg');
+            height: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            display: flex;
+            justify-content: center;
+            align-items: top;
+        }
+
+        .message {
+            text-align: center;
+            padding: 20px;
+            border-radius: 10px;
+            color: rgba(255, 255, 255, .8);
+            margin-top: 25vh
+        }
+    </style>
+</head>
+
+<body>
+    <div class="background">
+        <div class="message">
+            <h1>Wir sind gleich zurück</h1>
+            <p>Wir führen gerade Wartungsarbeiten durch. Bitte versuche es später erneut.</p>
+        </div>
+    </div>
+</body>
+
+</html>
