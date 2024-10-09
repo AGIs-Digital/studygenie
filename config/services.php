29a93b290f279cb4b55f07e4e450622b2e877005
Timo1002 <162825846+Timo1002@users.noreply.github.com>
2024-10-09 12:43:58 +0200

    google_directs

--- config/services.php
+++ config/services.php
@@ -34,6 +34,8 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_REDIRECT_URL'),
+        'redirect' => env('APP_NAME') === 'StudyGenie - Staging' 
+            ? env('STAGING_GOOGLE_REDIRECT_URL') 
+            : env('PRODUCTION_GOOGLE_REDIRECT_URL'),
     ],
 ];
