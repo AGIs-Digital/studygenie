5aa1e69fb5321087484b907804729f5059f666d2
Timo1002 <162825846+Timo1002@users.noreply.github.com>
2024-11-10 00:46:21 +0100

    Update wieso.php

--- resources/views/components/wieso.php
+++ resources/views/components/wieso.php
@@ -2,8 +2,9 @@
     <h2>Wie funktioniert StudyGenie?</h2>
     <div class="wiesoContentContainer">
         <p>Viele kennen LLMs (Large Language Models) wie ChatGPT der Firma OpenAI oder Gemini von Google als vielseitige KI-Tools. Bei StudyGenie haben wir diese Technologie speziell für Bildungs- und Karrierefragen angepasst, um Schülern und Studenten gezielt bei Lern- und Berufsfragen zu helfen.<br />
-        Unser Motto: "Hilfe zur Selbsthilfe"<br />
-         – statt nur Lösungen zu präsentieren. Zudem bietet StudyGenie die einfache Erstellung von Bewerbungsunterlagen nach DIN-Normen, sodass alle Werkzeuge für schulischen und beruflichen Erfolg an einem Ort verfügbar sind.<br /></p> 
-         <b>Weiterer Vorteil: Ihre Daten bleiben von Trainingszwecken ausgeschlossen.</b>
+        <div style="text-align: center;"><strong>Unser Motto: "Hilfe zur Selbsthilfe"</strong></div><br />
+         Statt einfach nur Lösungen zu präsentieren, hilft StudyGenie dabei, die eigenen Lösungen zu finden.<br />
+         Zudem bietet StudyGenie die einfache Erstellung von Bewerbungsunterlagen nach DIN-Normen, sodass alle Werkzeuge für schulischen und beruflichen Erfolg an einem Ort verfügbar sind.<br /></p> 
+         <b>Weiterer Vorteil: Alle Daten bleiben von Trainingszwecken ausgeschlossen.</b>
         </div>
 </section>
