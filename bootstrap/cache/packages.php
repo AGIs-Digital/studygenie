1731c7b53e10947c324cea385840719a7c0c5e73
Timo1002 <162825846+Timo1002@users.noreply.github.com>
2024-12-12 18:27:27 +0100

    update

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -87,13 +87,13 @@
   ),
   'spatie/laravel-ignition' => 
   array (
-    'providers' => 
-    array (
-      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
-    ),
     'aliases' => 
     array (
       'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
+    ),
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
     ),
   ),
   'spatie/laravel-signal-aware-command' => 
